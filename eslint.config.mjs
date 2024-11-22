abada634b89f89abf4be7ab73a946dc287bf0997
Wannes De Backer <wannes.debacker@craftzing.com>
2024-11-22 15:20:48 +0100

    Slight tweaks in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,8 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [{ languageOptions: { globals: { ...globals.node } } }];
+export default [
+  { languageOptions: { globals: globals.node }, ignores: ["test/*", "dist"] },
+  pluginJs.configs.recommended,
+];
